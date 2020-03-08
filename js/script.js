32ca2dccfa67278d394e1c2072b743f1acc95922
Alexis2005 <alexisescobar200513@gmail.com>
2020-03-07 18:20:57 -0600

    mercaderia

---
+++ js/script.js
@@ -0,0 +1,12 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBb7o2Sk31gblb0icSVlb4dkAGWw93D_hY",
+    authDomain: "maxapp-fe9e8.firebaseapp.com",
+    databaseURL: "https://maxapp-fe9e8.firebaseio.com",
+    projectId: "maxapp-fe9e8",
+    storageBucket: "maxapp-fe9e8.appspot.com",
+    messagingSenderId: "135200061544",
+    appId: "1:135200061544:web:8f76a3c1378405b43423dd",
+    measurementId: "G-EMQJYSBXLS"
+  };
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
